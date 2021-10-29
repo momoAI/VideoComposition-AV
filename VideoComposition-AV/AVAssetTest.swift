ff29ec214d74e823f99cc209e36f7f52d057ca43
luxu <luxu@fooww.com>
2021-10-29 15:25:37 +0800

    AVAsset

---
+++ VideoComposition-AV/AVAssetTest.swift
@@ -0,0 +1,76 @@
+//
+//  AVAssetTest.swift
+//  VideoComposition-AV
+//
+//  Created by luxu on 2021/10/29.
+//
+
+import Foundation
+import AVFoundation
+
+struct AVAssetTest {
+    static func test() {
+        // load http url
+        let url = URL(string: "https://test-img.fooww.com/groupxyz/M01/00/93/rBFpk2CLyBeALtPXAAi0ixJ8eVk433.mp4")
+    
+        guard let videoUrl = url else { return }
+        let asset = AVURLAsset(url: videoUrl)
+        
+        let duration = asset.duration
+        CMTimeShow(duration)
+        
+        let keys = ["duration"]
+        asset.loadValuesAsynchronously(forKeys: keys) {
+            let status = asset.statusOfValue(forKey: "duration", error: nil)
+            print(status.rawValue)
+        }
+        
+        // load file url
+        let url2 = Bundle.main.url(forResource: "video2.MP4", withExtension: nil)
+        guard let videoUrl = url2 else { return }
+        let asset2 = AVURLAsset(url: videoUrl)
+        
+        let duration2 = asset2.duration
+        CMTimeShow(duration2)
+        
+        // 65types utis
+        let types = AVURLAsset.audiovisualTypes()
+        print(types)
+        
+        // 44 mimetypes
+        let mimetypes = AVURLAsset.audiovisualMIMETypes()
+        print(mimetypes)
+        
+        print(asset.preferredRate, asset.preferredVolume)
+        print(asset2.preferredRate, asset2.preferredVolume)
+        
+        // track
+        // all tracks
+        let tracks = asset.tracks
+        let tracks2 = asset2.tracks
+        print(tracks,tracks2)
+        for track in tracks {
+            print(track.mediaType)
+        }
+        
+        // appoint mediatype tracks
+        let videoTracks = asset.tracks(withMediaType: .video)
+        print(videoTracks)
+        
+        let groups = asset2.trackGroups
+        for group in groups {
+            print(group.trackIDs)
+        }
+        
+        // Metadata
+        let createDate = asset2.creationDate
+        print(createDate?.stringValue ?? "")
+        print(asset2.lyrics ?? "")
+        let cmetadata = asset2.commonMetadata
+        let metadata = asset2.metadata
+        print(cmetadata,metadata)
+        
+        print(asset.containsFragments, asset2.containsFragments)
+        print(asset2.isPlayable,asset2.isExportable,asset2.isReadable,asset2.isComposable,asset2.isCompatibleWithSavedPhotosAlbum)
+    }
+}
---
+++ VideoComposition-AV/CMTimeTest.swift
@@ -0,0 +1,51 @@
+//
+//  CMTimeTest.swift
+//  VideoComposition-AV
+//
+//  Created by luxu on 2021/10/26.
+//
+
+import Foundation
+import CoreMedia
+
+struct CMTimeTest {
+    static func test() {
+        let time = CMTime(seconds: 0.5, preferredTimescale: 1)
+        print(time.seconds)
+        print(time.value)
+
+        let time2 = CMTime(seconds: 1, preferredTimescale: 2)
+        print(time2.seconds)
+        print(time2.value)
+
+        let time3 = CMTime(value: 1, timescale: 30)
+        print(time3.seconds)
+        print(time3.value)
+
+        let compassTime = time2 + time3
+        CMTimeShow(compassTime)
+
+        let zero = CMTime.zero
+        print(zero.seconds)
+        print(zero.value)
+        print(zero.timescale)
+
+        if zero < time3 {
+            print("min.....")
+        }
+        
+        let seconds = 200.0 / 3
+        //        let seconds = 66.667
+        let time4 = CMTime(seconds: seconds, preferredTimescale: 3)
+        CMTimeShow(time4)
+        let time5 = CMTimeMultiply(time4, multiplier: 3)
+        CMTimeShow(time5)
+        let time6 = time5 - CMTime(value: 200, timescale: 2)
+        CMTimeShow(time6)
+        
+        let range = CMTimeRange(start: time4, end: time5)
+        CMTimeRangeShow(range)
+        let range2 = CMTimeRange(start: time4, duration: time5)
+        CMTimeRangeShow(range2)
+    }
+}
